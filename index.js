155c52bd9cad1dc96e1fcbe067c34d88907535c5
nbaag <nbaagez@gmail.com>
2022-11-26 15:28:54 +0300

    fixes

--- index.js
+++ index.js
@@ -2,45 +2,39 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
-let time= 0;
-
-function setTime() {
-  time = inputEl.value;
-  let hours = Math.floor(time / 3600);
-  let minutes = Math.floor((time - (hours * 3600)) / 60);
-  let seconds = time - ((minutes * 60) + (hours * 3600));
-  // hours = hours < 10 ? "0" + hours : hours;
-  // minutes = minutes < 10 ? "0" + minutes : minutes;
-  // seconds = seconds < 10 ? "0" + seconds : seconds;
-
-  timerEl.innerHTML = hours + ":" + minutes + ":" + seconds;
-
-  let myInterval = setInterval(() => {
-    if(minutes === 0 && seconds === 0) {
-      hours--;
-      minutes = 59;
-      seconds = 60;
-    }
-
-    if(seconds === 0) {
-      minutes--;
-      seconds = 60;
-    }
-    
-    seconds--;
-    timerEl.innerHTML = hours + ":" + minutes + ":" + seconds;
-
-    if(hours === 0 && minutes === 0 && seconds === 0) {
-      clearInterval(myInterval)
-    }
-  }, 1000);
-}
-
-
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    let time = seconds;
+    
+    let timerHours = Math.floor(time / 3600);
+    let timerMinutes = Math.floor((time - (timerHours * 3600)) / 60);
+    let timerSeconds = time - ((timerMinutes * 60) + (timerHours * 3600));
+
+    timerEl.innerHTML = timerHours + ":" + timerMinutes + ":" + timerSeconds;
+
+    let myInterval = setInterval(() => {
+
+      if(timerMinutes === 0 && timerSeconds === 0) {
+        timerHours--;
+        timerMinutes = 59;
+        timerSeconds = 60;
+      }
+
+      if(timerSeconds === 0) {
+        timerMinutes--;
+        timerSeconds = 60;
+      }
+      
+      timerSeconds--;
+      timerEl.innerHTML = timerHours + ":" + timerMinutes + ":" + timerSeconds;
+
+      if(timerHours === 0 && timerMinutes === 0 && timerSeconds === 0) {
+        clearInterval(myInterval)
+      }
+    }, 1000);
+  };
 };
 
 const animateTimer = createTimerAnimator();
